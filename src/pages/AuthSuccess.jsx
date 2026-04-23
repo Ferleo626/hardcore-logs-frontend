0469b2741d3d39ec8dcdd45fb874961680fa5a0c
ferleo626 <patio.fernando9@gmail.com>
2026-04-23 10:35:51 -0300

    cambios de authsuccess

--- src/pages/AuthSuccess.jsx
+++ src/pages/AuthSuccess.jsx
@@ -1,19 +1,25 @@
 import { useEffect } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function AuthSuccess() {
-  const [params] = useSearchParams();
+
   const navigate = useNavigate();
 
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
     const token = params.get("token");
 
     if (token) {
       localStorage.setItem("token", token);
 
-      // 🔥 Redirección sin recargar la app
-      navigate("/", { replace: true });
+      console.log("✅ Token guardado");
+
+      navigate("/dashboard"); // o "/" o donde tengas la app
+    } else {
+      console.error("❌ No token");
+      navigate("/login");
     }
+
   }, []);
 
   return <h1>Conectando...</h1>;
