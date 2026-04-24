ea9cdad236882babba3e763044bc0db86670580c
ferleo626 <patio.fernando9@gmail.com>
2026-04-23 23:21:46 -0300

    console log para verificar muchos mas fallos

--- src/pages/AuthSuccess.jsx
+++ src/pages/AuthSuccess.jsx
@@ -10,7 +10,11 @@ export default function AuthSuccess() {
     const token = new URLSearchParams(window.location.search).get("token");
 
     console.log("TOKEN DETECTADO:", token);
+console.log("TOKEN GUARDADO ANTES:", localStorage.getItem("token"));
 
+localStorage.setItem("token", token);
+
+console.log("TOKEN GUARDADO DESPUÉS:", localStorage.getItem("token"));
     alert("TOKEN: " + token);
 
   }, []);
